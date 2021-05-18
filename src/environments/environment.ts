11996d279298dc513679f464af52c3d495895200
Rafael Batista <raphab33@hotmail.com>
2021-05-18 20:45:58 -0300

    Ativando modo teste

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,21 +1,8 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
   production: false,
   API_NODE_URL: "http://localhost:3000/api/v1",
-  PUBLIC_KEY_MERCADOPAGO: 'TEST-725fa0cb-1fb4-45d1-aa2e-44d731faf7d5',
-  ACCESS_TOKEN_MERCADOPAGO: 'TEST-1972903026303237-022601-d54dd51cc81683c3c5d683655ac00a11-168948207',
+  PUBLIC_KEY_MERCADOPAGO: 'TEST-64b01c45-bebe-4f98-b886-aca70613e67b',
+  ACCESS_TOKEN_MERCADOPAGO: 'TEST-6323426033293290-051100-e38be1556ac86e3fe559c706257e1a60-757039068',
   APP_ID_MERCADOPAGO: 6323426033293290,
   CLIENT_SECRET_MERCADOPAGO: 'XPAkdRIF387rqoYYScaBgGFv5TsRWm1a'
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
